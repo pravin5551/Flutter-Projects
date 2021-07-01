0ed4e1fa6c20707b785a8989523ac73946d7ed36
Pravin Pawar <72084029+pravin5551@users.noreply.github.com>
2021-07-01 10:49:29 +0530

    calcultor programm

---
+++ sprint-1/day-1/assignments/calcultor.dart
@@ -0,0 +1,40 @@
+
+void main() {
+  
+
+
+  int a = 5;
+
+  int b = 4;
+  
+  
+
+  print('Addition 1 | Sub 2 | Mul 3 | Div 4 ');
+  int op = 2;
+  
+  switch(op){
+      
+    case 1:
+      print('SUM is :${a+b}');
+      break;
+      
+      case 2:
+      print('Sub is :${a-b}');
+      break;
+      
+      case 3:
+      print('Mul is :${a*b}');
+      break;
+      
+      case 4:
+      print('Div is :${a/b}');
+      break;
+      
+    default:
+      print('Enter Valid Operation');
+      break;
+      
+      
+  }
+  
+}
