ffc521638e5f04d9cf0f8f42a43e28b912e47d55
pravin5551 <pravinvpawar5551@gmail.com>
2021-07-11 15:21:53 +0530

    API call first app

---
+++ sprint-3/day-4/we/async_we_flutter/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: NetworkCall(),
+    ),
+  ));
+}
+
+
+class NetworkCall extends StatefulWidget {
+  const NetworkCall({Key? key}) : super(key: key);
+
+  @override
+  _NetworkCallState createState() => _NetworkCallState();
+}
+
+class _NetworkCallState extends State<NetworkCall> {
+  String value ="20.0";
+  // int padding =20;
+  String dogImageURL = 'https://images.dog.ceo/breeds/schnauzer-miniature/n02097047_731.jpg';
+
+  double padding = 0.0;
+
+  void getRandomDog() async{
+    var url = Uri.parse('https://dog.ceo/api/breeds/image/random');
+   http.Response response= await http.get(url);
+   if(response.statusCode ==200){
+     var jsonData = convert.jsonDecode(response.body) as Map<String, dynamic>;
+
+     print(jsonData['message']);
+
+     setState(() {
+       dogImageURL = jsonData['message'];
+     });
+   }else{
+
+   }
+
+
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // getRandomDog();
+  }
+  @override
+  Widget build(BuildContext context) {
+   try{
+     padding = double.parse(value);
+   }catch(e){
+     padding = 30.0;
+   }
+
+   // getRandomDog();
+
+
+    return SafeArea(child: Center(
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            ElevatedButton(onPressed: (){
+              getRandomDog();
+            }, child: Text("Button")),
+            Expanded(child: Image(
+              image: NetworkImage(dogImageURL),
+            ))
+          ],
+        ),
+      ),
+    )
+    );
+  }
+}
+
+
---
+++ sprint-3/day-4/we/async_we_flutter/lib/test.dart
@@ -0,0 +1,27 @@
+
+
+
+import 'dart:io';
+//
+// main(List<String> args) async{
+//   work1();
+//   String? data2 =await work2() ;
+//   work3(data2!);
+// }
+//
+// void work1(){
+//   print("work1");
+// }
+//
+// Future<String?> work2() async{
+//   Duration duration = Duration(seconds : 3);
+//   await Future.delayed(duration);
+//   // sleep(duration);
+//   print("work2");
+//   return 'work 2 data';
+//
+// }
+//
+// void work3(String data2){
+//   print("work3 = $data2");
+// }
