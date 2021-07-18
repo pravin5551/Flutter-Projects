eaaaa0bf27c0b9b12ae4eeb410ca2bfea80bac19
pravin5551 <pravinvpawar5551@gmail.com>
2021-07-19 00:12:09 +0530

    Api List Practice - Model added

---
+++ api_flutter_eval/lib/Model/state_details_model.dart
@@ -0,0 +1,408 @@
+/// date : 20210307
+/// state : "AL"
+/// positive : 499819
+/// probableCases : 107742
+/// negative : 1931711
+/// pending : null
+/// totalTestResultsSource : "totalTestsPeopleViral"
+/// totalTestResults : 2323788
+/// hospitalizedCurrently : 494
+/// hospitalizedCumulative : 45976
+/// inIcuCurrently : null
+/// inIcuCumulative : 2676
+/// onVentilatorCurrently : null
+/// onVentilatorCumulative : 1515
+/// recovered : 295690
+/// lastUpdateEt : "3/7/2021 11:00"
+/// dateModified : "2021-03-07T11:00:00Z"
+/// checkTimeEt : "03/07 06:00"
+/// death : 10148
+/// hospitalized : 45976
+/// hospitalizedDischarged : null
+/// dateChecked : "2021-03-07T11:00:00Z"
+/// totalTestsViral : null
+/// positiveTestsViral : null
+/// negativeTestsViral : null
+/// positiveCasesViral : 392077
+/// deathConfirmed : 7963
+/// deathProbable : 2185
+/// totalTestEncountersViral : null
+/// totalTestsPeopleViral : 2323788
+/// totalTestsAntibody : null
+/// positiveTestsAntibody : null
+/// negativeTestsAntibody : null
+/// totalTestsPeopleAntibody : 119757
+/// positiveTestsPeopleAntibody : null
+/// negativeTestsPeopleAntibody : null
+/// totalTestsPeopleAntigen : null
+/// positiveTestsPeopleAntigen : null
+/// totalTestsAntigen : null
+/// positiveTestsAntigen : null
+/// fips : "01"
+/// positiveIncrease : 408
+/// negativeIncrease : 2087
+/// total : 2431530
+/// totalTestResultsIncrease : 2347
+/// posNeg : 2431530
+/// dataQualityGrade : null
+/// deathIncrease : -1
+/// hospitalizedIncrease : 0
+/// hash : "997207b430824ea40b8eb8506c19a93e07bc972e"
+/// commercialScore : 0
+/// negativeRegularScore : 0
+/// negativeScore : 0
+/// positiveScore : 0
+/// score : 0
+/// grade : ""
+
+class State_details_model {
+  int? _date;
+  String? _state;
+  int? _positive;
+  int? _probableCases;
+  int? _negative;
+  dynamic? _pending;
+  String? _totalTestResultsSource;
+  int? _totalTestResults;
+  int? _hospitalizedCurrently;
+  int? _hospitalizedCumulative;
+  dynamic? _inIcuCurrently;
+  int? _inIcuCumulative;
+  dynamic? _onVentilatorCurrently;
+  int? _onVentilatorCumulative;
+  int? _recovered;
+  String? _lastUpdateEt;
+  String? _dateModified;
+  String? _checkTimeEt;
+  int? _death;
+  int? _hospitalized;
+  dynamic? _hospitalizedDischarged;
+  String? _dateChecked;
+  dynamic? _totalTestsViral;
+  dynamic? _positiveTestsViral;
+  dynamic? _negativeTestsViral;
+  int? _positiveCasesViral;
+  int? _deathConfirmed;
+  int? _deathProbable;
+  dynamic? _totalTestEncountersViral;
+  int? _totalTestsPeopleViral;
+  dynamic? _totalTestsAntibody;
+  dynamic? _positiveTestsAntibody;
+  dynamic? _negativeTestsAntibody;
+  int? _totalTestsPeopleAntibody;
+  dynamic? _positiveTestsPeopleAntibody;
+  dynamic? _negativeTestsPeopleAntibody;
+  dynamic? _totalTestsPeopleAntigen;
+  dynamic? _positiveTestsPeopleAntigen;
+  dynamic? _totalTestsAntigen;
+  dynamic? _positiveTestsAntigen;
+  String? _fips;
+  int? _positiveIncrease;
+  int? _negativeIncrease;
+  int? _total;
+  int? _totalTestResultsIncrease;
+  int? _posNeg;
+  dynamic? _dataQualityGrade;
+  int? _deathIncrease;
+  int? _hospitalizedIncrease;
+  String? _hash;
+  int? _commercialScore;
+  int? _negativeRegularScore;
+  int? _negativeScore;
+  int? _positiveScore;
+  int? _score;
+  String? _grade;
+
+  int? get date => _date;
+  String? get state => _state;
+  int? get positive => _positive;
+  int? get probableCases => _probableCases;
+  int? get negative => _negative;
+  dynamic? get pending => _pending;
+  String? get totalTestResultsSource => _totalTestResultsSource;
+  int? get totalTestResults => _totalTestResults;
+  int? get hospitalizedCurrently => _hospitalizedCurrently;
+  int? get hospitalizedCumulative => _hospitalizedCumulative;
+  dynamic? get inIcuCurrently => _inIcuCurrently;
+  int? get inIcuCumulative => _inIcuCumulative;
+  dynamic? get onVentilatorCurrently => _onVentilatorCurrently;
+  int? get onVentilatorCumulative => _onVentilatorCumulative;
+  int? get recovered => _recovered;
+  String? get lastUpdateEt => _lastUpdateEt;
+  String? get dateModified => _dateModified;
+  String? get checkTimeEt => _checkTimeEt;
+  int? get death => _death;
+  int? get hospitalized => _hospitalized;
+  dynamic? get hospitalizedDischarged => _hospitalizedDischarged;
+  String? get dateChecked => _dateChecked;
+  dynamic? get totalTestsViral => _totalTestsViral;
+  dynamic? get positiveTestsViral => _positiveTestsViral;
+  dynamic? get negativeTestsViral => _negativeTestsViral;
+  int? get positiveCasesViral => _positiveCasesViral;
+  int? get deathConfirmed => _deathConfirmed;
+  int? get deathProbable => _deathProbable;
+  dynamic? get totalTestEncountersViral => _totalTestEncountersViral;
+  int? get totalTestsPeopleViral => _totalTestsPeopleViral;
+  dynamic? get totalTestsAntibody => _totalTestsAntibody;
+  dynamic? get positiveTestsAntibody => _positiveTestsAntibody;
+  dynamic? get negativeTestsAntibody => _negativeTestsAntibody;
+  int? get totalTestsPeopleAntibody => _totalTestsPeopleAntibody;
+  dynamic? get positiveTestsPeopleAntibody => _positiveTestsPeopleAntibody;
+  dynamic? get negativeTestsPeopleAntibody => _negativeTestsPeopleAntibody;
+  dynamic? get totalTestsPeopleAntigen => _totalTestsPeopleAntigen;
+  dynamic? get positiveTestsPeopleAntigen => _positiveTestsPeopleAntigen;
+  dynamic? get totalTestsAntigen => _totalTestsAntigen;
+  dynamic? get positiveTestsAntigen => _positiveTestsAntigen;
+  String? get fips => _fips;
+  int? get positiveIncrease => _positiveIncrease;
+  int? get negativeIncrease => _negativeIncrease;
+  int? get total => _total;
+  int? get totalTestResultsIncrease => _totalTestResultsIncrease;
+  int? get posNeg => _posNeg;
+  dynamic? get dataQualityGrade => _dataQualityGrade;
+  int? get deathIncrease => _deathIncrease;
+  int? get hospitalizedIncrease => _hospitalizedIncrease;
+  String? get hash => _hash;
+  int? get commercialScore => _commercialScore;
+  int? get negativeRegularScore => _negativeRegularScore;
+  int? get negativeScore => _negativeScore;
+  int? get positiveScore => _positiveScore;
+  int? get score => _score;
+  String? get grade => _grade;
+
+  State_details_model({
+      int? date, 
+      String? state, 
+      int? positive, 
+      int? probableCases, 
+      int? negative, 
+      dynamic? pending, 
+      String? totalTestResultsSource, 
+      int? totalTestResults, 
+      int? hospitalizedCurrently, 
+      int? hospitalizedCumulative, 
+      dynamic? inIcuCurrently, 
+      int? inIcuCumulative, 
+      dynamic? onVentilatorCurrently, 
+      int? onVentilatorCumulative, 
+      int? recovered, 
+      String? lastUpdateEt, 
+      String? dateModified, 
+      String? checkTimeEt, 
+      int? death, 
+      int? hospitalized, 
+      dynamic? hospitalizedDischarged, 
+      String? dateChecked, 
+      dynamic? totalTestsViral, 
+      dynamic? positiveTestsViral, 
+      dynamic? negativeTestsViral, 
+      int? positiveCasesViral, 
+      int? deathConfirmed, 
+      int? deathProbable, 
+      dynamic? totalTestEncountersViral, 
+      int? totalTestsPeopleViral, 
+      dynamic? totalTestsAntibody, 
+      dynamic? positiveTestsAntibody, 
+      dynamic? negativeTestsAntibody, 
+      int? totalTestsPeopleAntibody, 
+      dynamic? positiveTestsPeopleAntibody, 
+      dynamic? negativeTestsPeopleAntibody, 
+      dynamic? totalTestsPeopleAntigen, 
+      dynamic? positiveTestsPeopleAntigen, 
+      dynamic? totalTestsAntigen, 
+      dynamic? positiveTestsAntigen, 
+      String? fips, 
+      int? positiveIncrease, 
+      int? negativeIncrease, 
+      int? total, 
+      int? totalTestResultsIncrease, 
+      int? posNeg, 
+      dynamic? dataQualityGrade, 
+      int? deathIncrease, 
+      int? hospitalizedIncrease, 
+      String? hash, 
+      int? commercialScore, 
+      int? negativeRegularScore, 
+      int? negativeScore, 
+      int? positiveScore, 
+      int? score, 
+      String? grade}){
+    _date = date;
+    _state = state;
+    _positive = positive;
+    _probableCases = probableCases;
+    _negative = negative;
+    _pending = pending;
+    _totalTestResultsSource = totalTestResultsSource;
+    _totalTestResults = totalTestResults;
+    _hospitalizedCurrently = hospitalizedCurrently;
+    _hospitalizedCumulative = hospitalizedCumulative;
+    _inIcuCurrently = inIcuCurrently;
+    _inIcuCumulative = inIcuCumulative;
+    _onVentilatorCurrently = onVentilatorCurrently;
+    _onVentilatorCumulative = onVentilatorCumulative;
+    _recovered = recovered;
+    _lastUpdateEt = lastUpdateEt;
+    _dateModified = dateModified;
+    _checkTimeEt = checkTimeEt;
+    _death = death;
+    _hospitalized = hospitalized;
+    _hospitalizedDischarged = hospitalizedDischarged;
+    _dateChecked = dateChecked;
+    _totalTestsViral = totalTestsViral;
+    _positiveTestsViral = positiveTestsViral;
+    _negativeTestsViral = negativeTestsViral;
+    _positiveCasesViral = positiveCasesViral;
+    _deathConfirmed = deathConfirmed;
+    _deathProbable = deathProbable;
+    _totalTestEncountersViral = totalTestEncountersViral;
+    _totalTestsPeopleViral = totalTestsPeopleViral;
+    _totalTestsAntibody = totalTestsAntibody;
+    _positiveTestsAntibody = positiveTestsAntibody;
+    _negativeTestsAntibody = negativeTestsAntibody;
+    _totalTestsPeopleAntibody = totalTestsPeopleAntibody;
+    _positiveTestsPeopleAntibody = positiveTestsPeopleAntibody;
+    _negativeTestsPeopleAntibody = negativeTestsPeopleAntibody;
+    _totalTestsPeopleAntigen = totalTestsPeopleAntigen;
+    _positiveTestsPeopleAntigen = positiveTestsPeopleAntigen;
+    _totalTestsAntigen = totalTestsAntigen;
+    _positiveTestsAntigen = positiveTestsAntigen;
+    _fips = fips;
+    _positiveIncrease = positiveIncrease;
+    _negativeIncrease = negativeIncrease;
+    _total = total;
+    _totalTestResultsIncrease = totalTestResultsIncrease;
+    _posNeg = posNeg;
+    _dataQualityGrade = dataQualityGrade;
+    _deathIncrease = deathIncrease;
+    _hospitalizedIncrease = hospitalizedIncrease;
+    _hash = hash;
+    _commercialScore = commercialScore;
+    _negativeRegularScore = negativeRegularScore;
+    _negativeScore = negativeScore;
+    _positiveScore = positiveScore;
+    _score = score;
+    _grade = grade;
+}
+
+  State_details_model.fromJson(dynamic json) {
+    _date = json["date"];
+    _state = json["state"];
+    _positive = json["positive"];
+    _probableCases = json["probableCases"];
+    _negative = json["negative"];
+    _pending = json["pending"];
+    _totalTestResultsSource = json["totalTestResultsSource"];
+    _totalTestResults = json["totalTestResults"];
+    _hospitalizedCurrently = json["hospitalizedCurrently"];
+    _hospitalizedCumulative = json["hospitalizedCumulative"];
+    _inIcuCurrently = json["inIcuCurrently"];
+    _inIcuCumulative = json["inIcuCumulative"];
+    _onVentilatorCurrently = json["onVentilatorCurrently"];
+    _onVentilatorCumulative = json["onVentilatorCumulative"];
+    _recovered = json["recovered"];
+    _lastUpdateEt = json["lastUpdateEt"];
+    _dateModified = json["dateModified"];
+    _checkTimeEt = json["checkTimeEt"];
+    _death = json["death"];
+    _hospitalized = json["hospitalized"];
+    _hospitalizedDischarged = json["hospitalizedDischarged"];
+    _dateChecked = json["dateChecked"];
+    _totalTestsViral = json["totalTestsViral"];
+    _positiveTestsViral = json["positiveTestsViral"];
+    _negativeTestsViral = json["negativeTestsViral"];
+    _positiveCasesViral = json["positiveCasesViral"];
+    _deathConfirmed = json["deathConfirmed"];
+    _deathProbable = json["deathProbable"];
+    _totalTestEncountersViral = json["totalTestEncountersViral"];
+    _totalTestsPeopleViral = json["totalTestsPeopleViral"];
+    _totalTestsAntibody = json["totalTestsAntibody"];
+    _positiveTestsAntibody = json["positiveTestsAntibody"];
+    _negativeTestsAntibody = json["negativeTestsAntibody"];
+    _totalTestsPeopleAntibody = json["totalTestsPeopleAntibody"];
+    _positiveTestsPeopleAntibody = json["positiveTestsPeopleAntibody"];
+    _negativeTestsPeopleAntibody = json["negativeTestsPeopleAntibody"];
+    _totalTestsPeopleAntigen = json["totalTestsPeopleAntigen"];
+    _positiveTestsPeopleAntigen = json["positiveTestsPeopleAntigen"];
+    _totalTestsAntigen = json["totalTestsAntigen"];
+    _positiveTestsAntigen = json["positiveTestsAntigen"];
+    _fips = json["fips"];
+    _positiveIncrease = json["positiveIncrease"];
+    _negativeIncrease = json["negativeIncrease"];
+    _total = json["total"];
+    _totalTestResultsIncrease = json["totalTestResultsIncrease"];
+    _posNeg = json["posNeg"];
+    _dataQualityGrade = json["dataQualityGrade"];
+    _deathIncrease = json["deathIncrease"];
+    _hospitalizedIncrease = json["hospitalizedIncrease"];
+    _hash = json["hash"];
+    _commercialScore = json["commercialScore"];
+    _negativeRegularScore = json["negativeRegularScore"];
+    _negativeScore = json["negativeScore"];
+    _positiveScore = json["positiveScore"];
+    _score = json["score"];
+    _grade = json["grade"];
+  }
+
+  Map<String, dynamic> toJson() {
+    var map = <String, dynamic>{};
+    map["date"] = _date;
+    map["state"] = _state;
+    map["positive"] = _positive;
+    map["probableCases"] = _probableCases;
+    map["negative"] = _negative;
+    map["pending"] = _pending;
+    map["totalTestResultsSource"] = _totalTestResultsSource;
+    map["totalTestResults"] = _totalTestResults;
+    map["hospitalizedCurrently"] = _hospitalizedCurrently;
+    map["hospitalizedCumulative"] = _hospitalizedCumulative;
+    map["inIcuCurrently"] = _inIcuCurrently;
+    map["inIcuCumulative"] = _inIcuCumulative;
+    map["onVentilatorCurrently"] = _onVentilatorCurrently;
+    map["onVentilatorCumulative"] = _onVentilatorCumulative;
+    map["recovered"] = _recovered;
+    map["lastUpdateEt"] = _lastUpdateEt;
+    map["dateModified"] = _dateModified;
+    map["checkTimeEt"] = _checkTimeEt;
+    map["death"] = _death;
+    map["hospitalized"] = _hospitalized;
+    map["hospitalizedDischarged"] = _hospitalizedDischarged;
+    map["dateChecked"] = _dateChecked;
+    map["totalTestsViral"] = _totalTestsViral;
+    map["positiveTestsViral"] = _positiveTestsViral;
+    map["negativeTestsViral"] = _negativeTestsViral;
+    map["positiveCasesViral"] = _positiveCasesViral;
+    map["deathConfirmed"] = _deathConfirmed;
+    map["deathProbable"] = _deathProbable;
+    map["totalTestEncountersViral"] = _totalTestEncountersViral;
+    map["totalTestsPeopleViral"] = _totalTestsPeopleViral;
+    map["totalTestsAntibody"] = _totalTestsAntibody;
+    map["positiveTestsAntibody"] = _positiveTestsAntibody;
+    map["negativeTestsAntibody"] = _negativeTestsAntibody;
+    map["totalTestsPeopleAntibody"] = _totalTestsPeopleAntibody;
+    map["positiveTestsPeopleAntibody"] = _positiveTestsPeopleAntibody;
+    map["negativeTestsPeopleAntibody"] = _negativeTestsPeopleAntibody;
+    map["totalTestsPeopleAntigen"] = _totalTestsPeopleAntigen;
+    map["positiveTestsPeopleAntigen"] = _positiveTestsPeopleAntigen;
+    map["totalTestsAntigen"] = _totalTestsAntigen;
+    map["positiveTestsAntigen"] = _positiveTestsAntigen;
+    map["fips"] = _fips;
+    map["positiveIncrease"] = _positiveIncrease;
+    map["negativeIncrease"] = _negativeIncrease;
+    map["total"] = _total;
+    map["totalTestResultsIncrease"] = _totalTestResultsIncrease;
+    map["posNeg"] = _posNeg;
+    map["dataQualityGrade"] = _dataQualityGrade;
+    map["deathIncrease"] = _deathIncrease;
+    map["hospitalizedIncrease"] = _hospitalizedIncrease;
+    map["hash"] = _hash;
+    map["commercialScore"] = _commercialScore;
+    map["negativeRegularScore"] = _negativeRegularScore;
+    map["negativeScore"] = _negativeScore;
+    map["positiveScore"] = _positiveScore;
+    map["score"] = _score;
+    map["grade"] = _grade;
+    return map;
+  }
+
+}
