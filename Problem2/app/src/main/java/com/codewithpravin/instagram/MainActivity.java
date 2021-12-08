f01838e48308f81f2e1db13a791f07b4b06c1933
Pravin Pawar <72084029+pravin5551@users.noreply.github.com>
2021-12-08 16:07:43 +0530

    Update MainActivity.java
    Commented unsued one

--- Problem2/app/src/main/java/com/codewithpravin/instagram/MainActivity.java
+++ Problem2/app/src/main/java/com/codewithpravin/instagram/MainActivity.java
@@ -1,7 +1,7 @@
 package com.codewithpravin.instagram;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+//import android.os.Bundle;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
